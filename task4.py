e72373e49ab4184491d9c187daaa8be9fdef23e1
Mseserovich <tshepo.tshabalala@moov.life>
2020-10-24 09:39:07 +0200

    fixed bug/typecasting

--- task4.py
+++ task4.py
@@ -1,9 +1,9 @@
 def check_num(num1, num2):
     """Function that checks if either num is 3
-        AND their sum contains a 3"""
-    sum = num1 + num2
-    print(sum)
-    if (num1 == 3 or num2 == 3 and 3 in sum):
+        AND their sum contains a 3 then returns true"""
+        
+    sum = int(num1) + int(num2)
+    if (num1 == '3' or num2 == '3' and '3' in str(sum)):
         return True
     else:
         return False
