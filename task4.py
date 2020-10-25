0b7e727659c8988d3feda96ea162b714eb70249b
Mseserovich <tshepo.tshabalala@moov.life>
2020-10-25 06:42:17 +0200

    fixed operator precedence

--- task4.py
+++ task4.py
@@ -3,7 +3,7 @@ def check_num(num1, num2):
         AND their sum contains a 3 then returns true"""
         
     sum = int(num1) + int(num2)
-    if (num1 == '3' or num2 == '3' and '3' in str(sum)):
+    if ((num1 == '3' or num2 == '3') and ('3' in str(sum))):
         return True
     else:
         return False
